9f2d5e8e91d09befb25e72aec4a0aad10d092603
MTBonde <thorbonde@gmail.com>
2025-10-25 10:40:19 +0200

    bump RelayService to v0.2.1, update Dockerfile base image to aspnet, and adjust CD workflow

--- RelayService/ServiceVersion.cs
+++ RelayService/ServiceVersion.cs
@@ -8,7 +8,9 @@ public static class ServiceVersion
     /// <summary>
     /// Gets the current version of the RelayService in semantic versioning format (major.minor.patch)
     /// </summary>
-    public const string Current = "0.2.0";
+    public const string Current = "0.2.1";
 }
 
 // walking skeleton 0.1.0
+// websocket 0.2.0
+// dockerfile fix 0.2.1
